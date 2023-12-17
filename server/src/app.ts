b80a90beabc178c233ff5f68e30337e5a0cce969
nick <nick.flatow@gmail.com>
2023-12-17 16:49:01 -0600

    versioning

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,31 +4,29 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 
-import { planetsRouter } from './routes/planets/planets.router';
-import { launchesRouter } from './routes/launches/launches.router';
-import { dirname } from '../utils/meta'
-
 //TODO CHEAP FIX FOR JEST TS ISSUE should be import.meta.url
+import { dirname } from '../utils/meta'
 // const file = fileURLToPath("file:///C:/Users/Nick%20Flatow/Documents/code/nasa/server/src/app.ts");
 // const dir = path.dirname(file);
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
+
+
+import { api } from './routes/api';
+
+
 export const app = express();
 
 //add cors
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 app.use(cors());
 app.use(morgan('combined'));
  
 app.use(express.json());
 app.use(express.static(path.join(dirname, '..', 'public')));
-app.use('/planets',planetsRouter);
-app.use('/launches',launchesRouter);
+
+app.use('/v1', api);
+//to create v2 api
+// app.use('/v2', v2Router);
 
 // /* enbales react routing
 app.get('/*', (req, res) => {
