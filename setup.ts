64464cb38c80962ee47480dc3c68b286e937ad04
nick <nick.flatow@gmail.com>
2023-12-27 16:46:46 -0600

    added vitest

---
+++ setup.ts
@@ -0,0 +1,3 @@
+//load env variables for testing
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'server/.env'});
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    root:'.',
+    base: './',
+  test: {
+    // Adjust this pattern to match the location of your test files
+    include: ['server/src/routes/launches/__tests__/test.ts'],
+    exclude: ['node_modules', 'dist', /* other folders to exclude */],
+    setupFiles: ['./setup.ts'],
+  },
+});
